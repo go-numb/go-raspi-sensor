6c1dc0c0cffa347cc42352ac4a16a4d97837ca41
numbp <o9o73286699@gmail.com>
2019-11-15 02:23:08 +0900

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# Raspberry Pi 3 read sensor
+toggle switch if the humidity is high.
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"gobot.io/x/gobot/drivers/aio"
+	"gobot.io/x/gobot/platforms/firmata"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
+
+const (
+	GETSENSOR = 5
+)
+
+func init() {
+
+}
+
+type Client struct {
+	Bot *gobot.Robot
+}
+
+func NewClient() *Client {
+	// ラズパイ
+	r := raspi.NewAdaptor()
+	relay := gpio.NewGroveRelayDriver(r, "7")
+
+	// 湿度センサー
+	firmataAdaptor := firmata.NewAdaptor()
+	sensor := aio.NewAnalogSensorDriver(firmataAdaptor, "0")
+
+	// tick worker
+	toggle := func() {
+		gobot.Every(time.Duration(GETSENSOR)*time.Second, func() {
+			if !isWater(sensor) {
+				return
+			}
+			relay.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("onOff",
+		[]gobot.Connection{r, firmataAdaptor},
+		[]gobot.Device{relay, sensor},
+		toggle,
+	)
+
+	return &Client{
+		Bot: robot,
+	}
+}
+
+func main() {
+	done := make(chan bool)
+
+	client := NewClient()
+	client.Bot.Start()
+	defer client.Bot.Stop()
+
+	<-done
+}
+
+// isWater is
+// センサーのデータを読み取る
+// - 温度 & 湿度
+func isWater(sensor *aio.AnalogSensorDriver) bool {
+	val, err := sensor.Read()
+	if err != nil {
+		log.Println("Failed to read", err)
+		return false
+	}
+	cel := (5.0 * float64(val) * 100.0) / 1024
+	logrus.Infof("Raw-value:%d Celsius:%.2f", val, cel)
+
+	// 湿度が高ければreturn false
+	if 30 < cel {
+		return false
+	}
+
+	return true
+}
